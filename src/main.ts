ea0fa9f6e0fb6e7d6a4a22686deb2cce2246fa77
Jisan901 <jisuna901@gmail.com>
2026-06-06 17:27:44 +0600

    updated port

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
     credentials: true,
   });
   app.setGlobalPrefix('api/v1');
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3093);
 }
 bootstrap();
